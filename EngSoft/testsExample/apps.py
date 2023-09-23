7f1a2f6e541831623835a45ecb14972a37f5fe41
Rafael dos Santos Parisi <rafael.parisi@autaza.com>
2023-09-23 17:47:23 -0300

    Teste simples

---
+++ EngSoft/testsExample/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TestsexampleConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "testsExample"
---
+++ EngSoft/testsExample/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+# Create your tests here.
+class MyTestCase(TestCase):
+    def test_example(self):
+        self.assertEqual(1 + 1, 2)
