c0e27673252461325fdf9d28e8ba396e1039e2e5
HarrisonCaetanoCandido <caetanocandido.h@gmail.com>
2023-10-14 03:16:20 -0300

    primeira tentativa de integracao

--- EngSoft/home/static/js/script-home.js
+++ EngSoft/home/static/js/script-home.js
@@ -1,9 +1,38 @@
-// Processa o email
-function enviarEmail() {
+// Formata o email para o app de newsletter fazer o envio para a mailchimp
+function sendEmail() {
     var email = document.getElementById("email").value;
+    var subscribeEmailUrl = emailInput.getAttribute('data-subscribe-email-url');
 
-    // Apenas visualizo o email na tela, nada demais
-    alert("Email a ser enviado: " + email);
+    alert("email:" + email)
+
+    // Cria um objeto XMLHttpRequest para fazer a solicitação POST
+    var xhr = new XMLHttpRequest();
+
+    // Abre a solicitação POST para a URL definida
+    xhr.open('POST', subscribeEmailUrl, true);
+
+    // Define o cabeçalho X-CSRFToken para o token CSRF (substitua pelo valor real)
+    xhr.setRequestHeader('X-CSRFToken', csrf_token);
+
+    // Define uma função a ser chamada quando a solicitação for concluída
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          // Exibe um alerta se a solicitação for bem-sucedida
+          alert('Email enviado com sucesso!');
+        } else {
+          // Exibe um alerta se ocorrer um erro na solicitação
+          alert('Erro ao enviar o email');
+        }
+      }
+    };
+
+    // Cria um objeto FormData e anexa o valor do email a ele
+    var data = new FormData();
+    data.append('email', email);
+
+    // Envia a solicitação POST com os dados do FormData
+    xhr.send(data);
 }
 
 // Esconde a imagem quando o email estiver no input
