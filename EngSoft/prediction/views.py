1da17c432705f7f87f38f1ed29c60af8f5077378
Rafael dos Santos Parisi <rafael.parisi@autaza.com>
2023-11-01 14:46:20 -0300

    Predição ocorrendo

--- EngSoft/prediction/views.py
+++ EngSoft/prediction/views.py
@@ -1,25 +1,25 @@
+import traceback
 from django.shortcuts import render
 from .models import ModelImage
-from PIL import Image as imgPil
-from EngSoft.settings import BASE_DIR
-import tensorflow as tf
-import numpy as np
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.core.files.storage import FileSystemStorage
+from .utils import initPrediction
 
 # Load your TensorFlow model here
-modelo = tf.keras.models.load_model(f'{BASE_DIR}/prediction/model/meu_modelo.h5') 
 
 def image_view(request):
     # Your existing code for displaying the image
     image = ModelImage.objects.filter(name='example').first()
-    resultado_predicao = ''
+
+    resultado_predicao = 'None'
+
     if image:
         resultado_predicao = predicao(request)
 
     context = {'image': image, 'res_pred': resultado_predicao}
+
     return render(request, 'image_display.html', context)
 
 @require_http_methods(['POST'])
@@ -32,7 +32,7 @@ def upload_image(request):
 
     # Save the uploaded image to a temporary location
     fs = FileSystemStorage()
-    image_path = fs.save(uploaded_image_name, uploaded_image)
+    fs.save(uploaded_image_name, uploaded_image)
 
     # Send a response indicating successful upload
     return JsonResponse({'message': 'Imagem enviada com sucesso!'})
@@ -40,26 +40,23 @@ def upload_image(request):
 @csrf_exempt
 @require_http_methods(['POST'])
 def predicao(request):
-    if 'image' not in request.FILES:
-        return JsonResponse({'erro': 'Nenhuma imagem fornecida.'}, status=400)
+    try:
+        if 'image' not in request.FILES:
+            return JsonResponse({'erro': 'Nenhuma imagem fornecida.'}, status=400)
 
-    uploaded_image = request.FILES['image']
-    uploaded_image_name = uploaded_image.name
+        uploaded_image = request.FILES['image']
+        uploaded_image_name = uploaded_image.name
+        
+        # Save the uploaded image to a temporary location
+        fs = FileSystemStorage()
+        image_path = fs.save(uploaded_image_name, uploaded_image)
 
-    # Save the uploaded image to a temporary location
-    fs = FileSystemStorage()
-    image_path = fs.save(uploaded_image_name, uploaded_image)
-    imagem = imgPil.open(image_path)
+        label_and_score = initPrediction(image_path)
+        return JsonResponse({'result': label_and_score})
 
-    # Preprocess the image as necessary for your model
-    imagem = np.array(imagem)
-    imagem = imagem / 255.0  # Example normalization
-    imagem = np.expand_dims(imagem, axis=0)
+    except Exception as e:
+        # Log the exception
+        traceback.print_exc()  # This will print detailed exception information to the console
 
-    # Make the prediction
-    predicao = modelo.predict(imagem)
-
-    # Convert the prediction to text (adjust as necessary)
-    resultado_texto = str(predicao)
-
-    return JsonResponse({'result': resultado_texto})
+        # Return an error response
+        return JsonResponse({'erro': 'Ocorreu um erro interno no servidor.'})
