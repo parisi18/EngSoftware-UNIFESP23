3586356f07b06037c4a6e884bfb7ad0bf255c9fc
Rafael dos Santos Parisi <rafael.parisi@autaza.com>
2023-11-13 00:27:13 -0300

    Caso de registro inicial

---
+++ EngSoft/userRegister/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    # first_name = forms.CharField(max_length=30)
+    # last_name = forms.CharField(max_length=30)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        # password1 eh o password inicial
+        # password2 eh a confirmacao do password
+
+    def save(self, commit=True):
+        user = super(UserRegisterForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+        
---
+++ EngSoft/userRegister/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib.auth import login
+from django.contrib import messages
+# Create your views here.
+
+def register_request(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("home")
+        else:
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = UserRegisterForm()
+    return render(request=request, template_name="userRegister/register.html", context={"register_form":form})
+
