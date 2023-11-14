8c8c29b1fc3af10c776371d6b53c02dfadcfdf97
Rafael dos Santos Parisi <rafael.parisi@autaza.com>
2023-11-13 22:17:12 -0300

    Lint fix

--- EngSoft/userRegister/tests.py
+++ EngSoft/userRegister/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 
 class RegisterPageTests(TestCase):
     username = 'newuser'
