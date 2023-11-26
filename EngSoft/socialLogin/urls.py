6e4f3c2896e8cefcc908f5720006d28da737357f
Rafael dos Santos Parisi <rafael.parisi@autaza.com>
2023-11-26 12:28:54 -0300

    Lint fix

--- EngSoft/socialLogin/urls.py
+++ EngSoft/socialLogin/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path, include
-from django.conf import settings
+from django.urls import path
 from .views import login
 
 urlpatterns = [path("social", login, name="login")]
